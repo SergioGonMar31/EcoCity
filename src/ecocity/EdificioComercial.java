80529557309a2c3a4dd28672ddc94ed2b22e70b7
Miguel <miguelbedmards@gmail.com>
2026-03-20 11:13:18 +0100

    Se ha implementado edificio y la interfaz Mantenible

--- src/ecocity/EdificioComercial.java
+++ src/ecocity/EdificioComercial.java
@@ -1,12 +1,30 @@
 package ecoCity;
 
-public class EdificioComercial {
-
+public class EdificioComercial extends Edificio implements Mantenible {
+	private Edificio edificio;
+	private int felicida;
+	private int cliente;
+	
 	public EdificioComercial() {
+
+	}
+
+	@Override
+	public void aplicarEfectoMensual() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void reparar() {
+		// TODO Auto-generated method stub
 		
 	}
-	
-	
-	
-	
+
+	@Override
+	public void desgastar(int cantidad) {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
