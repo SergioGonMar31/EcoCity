4634bd46586740db62408a6e138bb0220708d2f5
Miguel <miguelbedmards@gmail.com>
2026-03-20 11:26:01 +0100

    Se ha creado los getters y setter y sean terminado los metodos implementado de reparar y desgastar

--- src/ecocity/EdificioComercial.java
+++ src/ecocity/EdificioComercial.java
@@ -1,30 +1,64 @@
 package ecoCity;
 
 public class EdificioComercial extends Edificio implements Mantenible {
-	private Edificio edificio;
+	// ATRIBUTOS
 	private int felicida;
 	private int cliente;
-	
-	public EdificioComercial() {
 
+	// CONSTRUCTOR
+	public EdificioComercial(String nombre, int consumoEnergia, int costo) {
+		this.nombre = nombre;
+		this.consumoEnergia = consumoEnergia;
+		this.costo = costo;
+		this.felicida = 10;
+		this.salud = 100;
+		this.activo = true;
 	}
 
+	// METODOS HEREDADO Y IMPLEMENTADOS
 	@Override
-	public void aplicarEfectoMensual() {
-		// TODO Auto-generated method stub
+	public void aplicarEfectoMensual() { // Este metodo se encarga de aplicar un efecto mensual de felicidad mientras
+											// este se encuentre activo
+		if (activo && salud > 0) {
+
+		}
 
 	}
 
 	@Override
 	public void reparar() {
-		// TODO Auto-generated method stub
-		
+		salud = 100;
+
 	}
 
 	@Override
 	public void desgastar(int cantidad) {
-		// TODO Auto-generated method stub
+		salud -= cantidad;
 		
+		if(salud<=0) {
+			salud=0;
+			activo=false;
+			
+			
+		}
+	}
+
+	// GETTERS Y SETTERS
+
+	public int getFelicida() {
+		return felicida;
+	}
+
+	public void setFelicida(int felicida) {
+		this.felicida = felicida;
+	}
+
+	public int getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(int cliente) {
+		this.cliente = cliente;
 	}
 
 }
